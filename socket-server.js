0f534f20eb700b320d2e90eb5e7bf904bbdf36eb
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2017-09-29 17:51:03 +0530

    all modifications for chat

--- socket-server.js
+++ socket-server.js
@@ -1,12 +1,16 @@
 // Require HTTP module (to start server) and Socket.IO
-var http = require('http'), fs = require('fs'), io = require('socket.io');
+var http = require('http'),
+    fs = require('fs'),
+    io = require('socket.io');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+var serve = serveStatic("./");
+var mongoose = require('mongoose');
 
-// Start the server at port 8080
-var server = http.createServer(function(req, res){ 
-	
-
-	  if (req.url === "/") {
-        fs.readFile("socket-client.html", function (error, pgResp) {
+// Start the server at port 3000
+var server = http.createServer(function(req, res) {
+    if (req.url === "/") {
+        fs.readFile("socket-client.html", function(error, pgResp) {
             if (error) {
                 res.writeHead(404);
                 res.write('Contents you are looking are Not Found');
@@ -14,36 +18,59 @@ var server = http.createServer(function(req, res){
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.write(pgResp);
             }
-             
+
             res.end();
         });
     } else {
-        //4.
-       // Send HTML headers and message
-		res.writeHead(200,{ 'Content-Type': 'text/html' }); 
-		res.end('<h1>Hello Socket Lover!</h1>');
+        var done = finalhandler(req, res);
+        serve(req, res, done);
     }
 });
-server.listen(8080);
-  
+
+
+server.listen(3000);
+
 // Create a Socket.IO instance, passing it our server
 var socket = io.listen(server);
+var clients = 0;
+var clientid;
 
 // Add a connect listener
-socket.on('connection', function(client){ 
-	
-	// Create periodical which ends a message to the client every 5 seconds
-	var interval = setInterval(function() {
-		client.send('This is a message from the server!  ' + new Date());
-	},5000);
-	
-	// Success!  Now listen to messages to be received
-	client.on('message',function(event){ 
-		console.log('Received message from client!',event);
-	});
-	client.on('disconnect',function(){
-		clearInterval(interval);
-		console.log('Server has disconnected');
-	});
-	
+socket.on('connection', function(client) {
+    ++clients;
+    clientid = client.id;
+
+    // Success!  Now listen to messages to be received
+    client.on('message', function(event) {
+        var newMsg = new Chat({ msg: '' + event, senderid: '' + clientid });
+        newMsg.save(function(err){
+            console.log('saved, err = ' + err);
+            if(err) throw err;
+            console.log('echoeing back data =' + event);
+            socket.emit('customevent', event);
+        });
+    });
+
+    socket.emit('ucount', clients + " active connections");
+
+    client.on('disconnect', function() {
+        --clients;
+        console.log('Server has disconnected');
+    });
+});
+
+var chatSchema = mongoose.Schema({
+    msg: String,
+    senderid: String,
+    created: { type: Date, default: Date.now }
+});
+
+var Chat = mongoose.model('Message', chatSchema);
+
+mongoose.connect('mongodb://localhost:27017/chatdb', function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to mongodb!');
+    }
 });
